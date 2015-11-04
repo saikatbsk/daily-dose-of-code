f96f5465fe6c61c78186ebe99817c1139349ba25
saikatbsk <saikatbsk@gmail.com>
2015-11-04 18:49:53 +0530

    added new code

--- ImageProcessing/OpenCV_CPP/zz_trainCascade/main.cpp
+++ ImageProcessing/OpenCV_CPP/zz_trainCascade/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include "traincascade.h"
+
+CascadeClassifier cascade;
+
+void detectAndDisplay(Mat frame, CascadeClassifier cascade);
 
 int main(int argc, char * argv[]) {
     
