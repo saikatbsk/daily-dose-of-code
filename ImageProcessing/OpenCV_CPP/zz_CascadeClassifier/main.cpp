51ea46ba8cdf13db154c0ea448af0faefe60545f
saikatbsk <saikatbsk@gmail.com>
2015-11-05 01:37:03 +0530

    modified

--- ImageProcessing/OpenCV_CPP/zz_CascadeClassifier/main.cpp
+++ ImageProcessing/OpenCV_CPP/zz_CascadeClassifier/main.cpp
@@ -4,9 +4,27 @@
 using namespace std;
 using namespace cv;
 
-// String cascadename = "classifiers/banana_classifier.xml";
-String cascadename = "classifiers/haarcascade_frontalface_default.xml";
-CascadeClassifier cascade;
+String cascades[100] = {
+    "classifiers/banana_classifier.xml",
+    "classifiers/haarcascade_frontalface_default.xml",
+    "classifiers/classifier_WallClock.xml",
+    "classifiers/haarcascade_fullbody.xml",
+    "classifiers/haarcascade_upperbody.xml"
+};
+
+String cascade_names[100] = {
+    "Banana", "Face", "WallClock", "Human", "UpperBody"
+};
+
+Scalar color[100] = {
+    Scalar(0, 255, 255),
+    Scalar(255, 255, 0),
+    Scalar(255, 0, 0),
+    Scalar(0, 255, 0),
+    Scalar(0, 0, 255)
+};
+
+int objcount[100];
 
 void detectAndDisplay(Mat frame) {
     vector<Rect> objects;
@@ -18,28 +36,35 @@ void detectAndDisplay(Mat frame) {
     cvtColor(frame, frame_gray, CV_BGR2GRAY);
     equalizeHist(frame_gray, frame_gray);
 
+    cout << "Detected objects: ";
+
     /* Detect objects */
-    cascade.detectMultiScale(frame_gray   /* Matrix of the type CV_8U */,
-                             objects      /* Vector of rectangles */,
-                             1.1          /* scaleFactor: how much the image size is reduced at each image scale */,
-                             2            /* minNeighbors: how many neighbors each candidate rectangle should have */,
-                             0,
-                             Size(10, 10) /* Objects smaller than the size are ignored */
-                            );
+    for(int i = 0 ; i < 5 ; i++) {
+        CascadeClassifier cascade;
+        cascade.load(cascades[i]);
+        cascade.detectMultiScale(frame_gray, objects, 1.1, 2, 0, Size(80, 80));
 
-    /* Draw rectangles around detected objects */
-    for(size_t i = 0 ; i < objects.size() ; i++) {
-        Point pt1 = Point(objects[i].x, objects[i].y);
-        Point pt2 = Point(objects[i].x + objects[i].width, objects[i].y + objects[i].height);
+        /* Draw rectangles around detected objects */
+        for(size_t j = 0 ; j < objects.size() ; j++) {
+            Point pt1 = Point(objects[j].x, objects[j].y);
+            Point pt2 = Point(objects[j].x + objects[j].width, objects[j].y + objects[j].height);
 
-        rectangle(frame, pt1, pt2, Scalar(255, 255, 0), 2, 8, 0);
+            rectangle(frame, pt1, pt2, color[i], 2, 8, 0);
+            putText(frame, cascade_names[i], pt1, FONT_HERSHEY_SIMPLEX, 0.8, color[i], 1, CV_AA);
+        }
+
+        objcount[i] = objects.size();
+        if(objcount[i] > 0)
+            cout << cascade_names[i] << "(" << objcount[i] << ") ";
     }
+
+    cout << endl;
 
     imshow("Result - CascadeClassifier", frame);
 }
 
 int main(int argc, char * argv[]) {
-    int k;
+    int i, k;
 
     Mat src;
 
@@ -52,12 +77,6 @@ int main(int argc, char * argv[]) {
     /* Load input image */
     if((src = imread(argv[1])).empty()) {
         cerr << "---> Error loading image!\n";
-        return -1;
-    }
-
-    /* Load cascade classifier */
-    if(!(cascade.load(cascadename))) {
-        cerr << "---> Error loading cascade!\n";
         return -1;
     }
 
