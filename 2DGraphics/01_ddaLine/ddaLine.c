e7c27fa0a336f7041bdd65af92b235ac6853cd52
saikatbsk <saikatbsk@gmail.com>
2015-12-15 19:23:44 +0530

    modified code

--- 2DGraphics/01_ddaLine/ddaLine.c
+++ 2DGraphics/01_ddaLine/ddaLine.c
@@ -1,28 +1,41 @@
 #include "ddaLine.h"
 
+/*
+ * Digital Differential Analyzer  is a scan-conversion line algorithm
+ * based on calculating either dx or dy.  We sample the line  at unit
+ * intervals, and calculate the corresponding interger values nearest
+ * to line path in the other coordinate.
+ */
 void ddaLine(int x0, int y0, int x1, int y1, int color) {
     int   dx, dy, steps, i;
-    float x, y, xInc, yInc;
+    float xInc, yInc, x, y;
 
-    /* Compute the change along x-axis and y-axis */
+    /*
+     * Calculate displacement  along x,  and y-axis
+     * To handle  negative  changes we  do not take
+     * ablosute values here
+     */
     dx = x1 - x0;
     dy = y1 - y0;
 
-    /* Compute the deflection for both x and y at each step */
+    /* Determine incremental change along both axis */
     if(abs(dx) > abs(dy))
-        steps = abs(dx);
+        steps = dx;
     else
-        steps = abs(dy);
+        steps = dy;
 
     xInc = dx / (float)steps;
     yInc = dy / (float)steps;
 
-    /* Set the initial values of x and y */
-    x = x0; y = y0;
+    /*
+     * Draw the initial point   on screen which  is
+     * (x0 , y0). We don't care about the direction
+     * of propagation of the line as we have  taken
+     * care of the  negative changes  in dx, and dy
+     */
+    putpixel(ROUND(x0), ROUND(y0), color);
 
-    /* Plot the line starting from (x0, y0) to (x1, y1) */
-    putpixel(ROUND(x), ROUND(y), color);
-
+    /* Plot, and draw  successive points  on screen */
     for(i = 0 ; i < steps ; i++) {
         x += xInc;
         y += yInc;
