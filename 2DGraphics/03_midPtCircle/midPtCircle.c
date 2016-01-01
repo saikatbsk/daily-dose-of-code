6c63bdf07daeb9d79164015cb91227b4d869a17d
saikatbsk <saikatbsk@gmail.com>
2016-01-01 22:38:15 +0530

    added/modified comments

--- 2DGraphics/03_midPtCircle/midPtCircle.c
+++ 2DGraphics/03_midPtCircle/midPtCircle.c
@@ -4,15 +4,23 @@ void midPtCircle(int xCenter, int yCenter, int radius, int color) {
     int x, y, p;
     void plotCirclePoints(int, int, int, int, int);
 
-    /* Initial values of x, y, and decision parameter p */
+    /*
+     * Calculate the initial values of x, y, and decision parameter p
+     */
     x = 0;
     y = radius;
     p = 1 - radius;
 
-    /* Plot the initial points */
+    /*
+     * Draw the initial circle points using  8-way symmetry of circle
+     */
     plotCirclePoints(xCenter, yCenter, x, y, color);
 
-    /* Plot the remaining circle points */
+    /*
+     * Draw the remaining circle points for x < y; use decision param
+     * to evaluate  y values  for each scan line, and re-evaluate the
+     * decision parameter. Draw  circle points  using  8-way symmetry
+     */
     while(x < y) {
         x++;
 
