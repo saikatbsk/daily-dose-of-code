b96a618189ff0c1218ec6021adf1c6ac06c32efb
saikatbsk <saikatbsk@gmail.com>
2016-01-09 21:51:08 +0530

    added cpp code

---
+++ Home_Assignments/01_student_information_system/CPP/sis.hpp
@@ -0,0 +1,19 @@
+#ifndef _SIS_HPP_
+#define _SIS_HPP_
+
+#include <string>
+#define MAXSTUDENTS 128
+
+class Student {
+public:
+    std::string name;
+    std::string dept;
+    int year;
+    int classRoll;
+    int sub1;
+    int sub2;
+    int sub3;
+    int sub4;
+};
+
+#endif
