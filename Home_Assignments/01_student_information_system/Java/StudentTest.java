808a71d55785925924ffe348920390a00e4c3553
saikatbsk <saikatbsk@gmail.com>
2016-01-11 10:31:52 +0530

    Fixed Indentation

--- Home_Assignments/01_student_information_system/Java/StudentTest.java
+++ Home_Assignments/01_student_information_system/Java/StudentTest.java
@@ -2,19 +2,19 @@ import java.io.*;
 
 public class StudentTest {
     public static void main(String [] args) {
-      Student stu1 = new Student("Avishikta Lodh");
-      Student stu2 = new Student("Upasana Maity");
+        Student stu1 = new Student("Avishikta Lodh");
+        Student stu2 = new Student("Upasana Maity");
 
-      stu1.Dept("IT");
-      stu1.Year(1);
-      stu1.Roll(13);
-      stu1.Marks(78, 87);
-      stu1.Display();
+        stu1.Dept("IT");
+        stu1.Year(1);
+        stu1.Roll(13);
+        stu1.Marks(78, 87);
+        stu1.Display();
 
-      stu2.Dept("MMD");
-      stu2.Year(1);
-      stu2.Roll(5);
-      stu2.Marks(87, 78);
-      stu2.Display();
+        stu2.Dept("MMD");
+        stu2.Year(1);
+        stu2.Roll(5);
+        stu2.Marks(87, 78);
+        stu2.Display();
     }
 }
