083520976eb0a502a572f6ad9fd8eaa31eedcb41
saikatbsk <saikatbsk@gmail.com>
2016-03-24 21:41:03 +0530

    removed testbed

--- Home_Assignments/Java/00_testbed/TestBed.java
+++
@@ -1,7 +0,0 @@
-class TestBed {
-    //-- This is the TestBed class
-    public static void main(String [] args]) {
-    //-- This is the main method
-        System.out.println("Hello World!\n");
-    }
-}
