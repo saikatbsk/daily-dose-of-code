6d3f5a511ee83f9ecd08d2661d4f785e492bc85b
saikatbsk <saikatbsk@gmail.com>
2016-03-24 20:08:00 +0530

    added java

---
+++ Home_Assignments/Java/00_testbed/TestBed.java
@@ -0,0 +1,7 @@
+class TestBed {
+    //-- This is the TestBed class
+    public static void main(String [] args]) {
+    //-- This is the main method
+        System.out.println("Hello World!\n");
+    }
+}
