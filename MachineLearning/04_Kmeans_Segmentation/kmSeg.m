798246ac94e0959ba89ff46a96fe44a7ed1f9347
saikatbsk <saikatbsk@gmail.com>
2016-11-09 00:22:58 +0530

    modified dataset

--- MachineLearning/04_Kmeans_Segmentation/kmSeg.m
+++ MachineLearning/04_Kmeans_Segmentation/kmSeg.m
@@ -1,20 +1,16 @@
-im = imread('mango2.jpg');
-imFlat = double(reshape(im, size(im,1) * size(im,2), 3));
+imSrc  = imread('mango.jpg');
+
+imInp  = imresize(imSrc, .15);
+imFlat = double(reshape(imInp, size(imInp, 1) * size(imInp, 2), 3));
 
 K = 2;
-[kIDs, kC] = kmeans(imFlat, K, 'Display', 'iter', 'MaxIter', 1000, 'Start', 'sample');
+[kIdx kC] = kmeans(imFlat, K, 'Display', 'iter', 'MaxIter', 1000, 'Start', 'sample');
 
-imOut = reshape(uint8(kIDs), size(im,1), size(im,2));
+imMask = (reshape(uint8(kIdx), size(imInp, 1), size(imInp, 2))) - 1;
+imMask = imresize(imMask, [size(imSrc, 1), size(imSrc, 2)]);
 
-mask = imOut-1;
-imOrig = imread('mango1.jpg');
-mask = imresize(mask, [size(imOrig, 1), size(imOrig, 2)]);
+imOut1 = bsxfun(@times, imSrc, cast(imMask, class(imSrc)));
+imOut2 = bsxfun(@times, imSrc, cast(~imMask, class(imSrc)));
 
-masked1 = bsxfun(@times, imOrig, cast(mask, class(imOrig)));
-masked2 = bsxfun(@times, imOrig, cast(~mask, class(imOrig)));
-
-imwrite(masked1, 'out1.jpg');
-imwrite(masked2, 'out2.jpg');
-
-subplot(1, 2, 1); imshow(masked1);
-subplot(1, 2, 2); imshow(masked2);
+subplot(1, 2, 1); imshow(imOut1);
+subplot(1, 2, 2); imshow(imOut2);
