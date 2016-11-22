484ace5884adc2a8b8a6eadce0f5c06359c35327
saikatbsk <saikatbsk@gmail.com>
2016-11-22 08:19:26 +0530

    sync

--- MachineLearning/05_Softmax/softmax_demo.m
+++ MachineLearning/05_Softmax/softmax_demo.m
@@ -1,3 +1,13 @@
+% Softmax demo
+
+function main
+    y = [1, 2, 3, 6; 2, 4, 5, 6; 3, 8, 7, 6];
+    s = softmax(y);
+
+    subplot(2, 1, 1); bar(y'); ylabel('scores');
+    subplot(2, 1, 2); bar(s'); ylabel('probabilities');
+end
+
 % The softmax function turns scores into probabilities.
 % General algorithm,
 %       a = softmax(n) = exp(n)/sum(exp(n))
--- MachineLearning/05_Softmax/test.m
+++
@@ -1,7 +0,0 @@
-clear; clc;
-
-y = [1, 2, 3, 6; 2, 4, 5, 6; 3, 8, 7, 6];
-s = softmax(y);
-
-subplot(2, 1, 1); bar(y'); ylabel('scores');
-subplot(2, 1, 2); bar(s'); ylabel('probabilities');
